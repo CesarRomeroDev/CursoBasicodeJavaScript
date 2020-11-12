5377811e8accf4075aaf0914c4883438924ec494
cesar <cesar_romero.dev@outlook.com>
2020-11-12 13:38:42 -0600

    CursoBasicoJS_12/11/2020

---
+++ valores.js
@@ -0,0 +1,26 @@
+//Tipos Primitivos
+
+40 // entero
+
+"julio cesar" //string "cadena de textos"
+
+true    //tipo boolean  verdadero
+false    //tipo boolean falso
+
+null              //valores placeholder valores que se guardan en memoria
+undefined         //valores placeholder valores que se guardan en memoria
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+
+//valores tipo objeto
+
+[1,2,3] //tipo arrays
+
+{nombre: "julio"}  //tipo objeto
+
+
+/////////////////////////////////////////////////////////////////////////
+
+//vamos a consola y con la palabra reservada "typeof" y en seguida un valor ya sea entero "40" la consola te 
+//muestra el tipo de valor "number"
---
+++ variables.js
@@ -0,0 +1,18 @@
+var nombre = "julio";
+
+// hay dos formas de declarar una variable
+var edad;
+//inicializar
+edad = 30;
+
+edad
+
+// se pueden generar variables mas complejas
+var elementos = ["computadora", "celular"];
+
+var persona = {          //tipo objeto
+    nombre: "julio",
+    edad: 30
+}
+
+persona;
