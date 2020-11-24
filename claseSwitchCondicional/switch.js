42266bf6fc0bae497ee96cd0f0f59c8987cf9442
cesar <cesar_romero.dev@outlook.com>
2020-11-23 23:15:46 -0600

    BasicoJavaScript

---
+++ claseSwitchCondicional/switch.js
@@ -0,0 +1,15 @@
+var numero = 0;
+
+        switch(numero){
+            case 1:
+                console.log("soy un uno!");
+                break;
+            case 10:
+                console.log("soy un 10!");
+                break;
+            case 100:
+                console.log("si soy un 100!");
+                break;
+            default:
+                console.log("no soy nada");
+        }
