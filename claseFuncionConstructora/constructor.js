10735bdd496a3bd55ded5be6eca2b9fdda03412f
cesar <cesar_romero.dev@outlook.com>
2020-12-08 22:29:02 -0600

    terminacionCursoBasicoJavaScript

---
+++ claseFuncionConstructora/constructor.js
@@ -0,0 +1,11 @@
+function auto(marca, modelo, annio){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+}
+
+var autoNuevo = new auto("Tesla", "Modelo 3", 2020);
+
+
+var autoNuevo2 = new auto("Nissan", "Modelo 4", 1920);
+var autoNuevo2 = new auto("Toyota", "Corolla", 1920);
